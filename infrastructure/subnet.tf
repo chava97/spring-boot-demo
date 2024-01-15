612097aa2dd2c23181effc401243a49544929248
chava-c <scardenas@griddynamics.com>
2024-01-15 05:30:17 -0600

    changing gke setup

--- infrastructure/subnet.tf
+++ infrastructure/subnet.tf
@@ -2,5 +2,5 @@ resource "google_compute_subnetwork" "subnet" {
     name                        = "private"
     ip_cidr_range               = "10.0.0.0/24"
     region                      = var.region
-    network                     = google_compute_network.demo.id
+    network                     = google_compute_network.vpc.id
 }

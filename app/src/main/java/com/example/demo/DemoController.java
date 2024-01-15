469c03d1e2ad9328083fbe1410a4808bbe230bcb
chava-c <scardenas@griddynamics.com>
2024-01-15 14:33:29 -0600

    testing workflow

--- app/src/main/java/com/example/demo/DemoController.java
+++ app/src/main/java/com/example/demo/DemoController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class DemoController {
     @GetMapping("/hello")
     public String hello(){
-        return "hello world!";
+        return "hello world";
     }
 }
 
